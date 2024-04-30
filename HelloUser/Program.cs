89d39c930b8cc7cc9d3a2ffd493dfe71b4b5f180
illavarslande <illavarslande@gmail.com>
2024-05-01 00:46:09 +0500

    Задача "Приветствие".

---
+++ HelloUser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, " + name + "!");
+        }
+    }
+}
