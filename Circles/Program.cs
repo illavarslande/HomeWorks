74697f2e10be159006ec302cfb87ab6e2d22d169
illavarslande <illavarslande@gmail.com>
2024-05-04 23:17:19 +0500

    Убрала \n в Console.WriteLine, чтобы код выглядел понятнее.

--- Circles/Program.cs
+++ Circles/Program.cs
@@ -9,15 +9,17 @@ namespace Circles
             double radiusFirstCircle = 5;
             double areaFirstCircle = Math.PI * Math.Pow(radiusFirstCircle, 2);
             double circumferenceFirstArea = 2 * Math.PI * radiusFirstCircle;
-            Console.WriteLine("Радиус первой окружнсти равен " + radiusFirstCircle + ".\n Её площадь равна " +  areaFirstCircle + 
-                ".\n Длина первой окружности равна " + circumferenceFirstArea + ".");
+            Console.WriteLine("Радиус первой окружнсти равен " + radiusFirstCircle);
+            Console.WriteLine("Её площадь равна " + areaFirstCircle);
+            Console.WriteLine("Длина первой окружности равна " + circumferenceFirstArea);
 
             double areaSecondCircle = 40;
             double angleSectorSecondCircle = 45;
             double radiusSecondArea = Math.Sqrt(areaSecondCircle / Math.PI);
             double areaSectorSecondArea = (Math.PI * Math.Pow(radiusSecondArea, 2)) / 360 * angleSectorSecondCircle;
-            Console.WriteLine("Площадь второй окружности равна " + areaSecondCircle + ".\n Радиус второй окружности равен " + radiusSecondArea + 
-                ".\n Площадь сектора с углом в " + angleSectorSecondCircle + " градусов равна " + areaSectorSecondArea + ".");
+            Console.WriteLine("Площадь второй окружности равна " + areaSecondCircle);
+            Console.WriteLine("Радиус второй окружности равен " + radiusSecondArea);
+            Console.WriteLine("Площадь сектора с углом в " + angleSectorSecondCircle + " градусов равна " + areaSectorSecondArea);
         }
     }
 }
