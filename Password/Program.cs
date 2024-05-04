85848900e6e0af20bee8cd2289dcd27c67aff873
illavarslande <illavarslande@gmail.com>
2024-05-04 23:11:15 +0500

    Задачка "Пароль".

---
+++ Password/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "LagertaSuperCat!";
+            string passwordInput = Console.ReadLine();
+            int numberCharactersInPassword = password.Length;
+            int numberCharactersInPasswordInput = passwordInput.Length;
+            if (password.Equals(passwordInput))
+            {
+                Console.WriteLine("Пароль верный!");
+            }
+            else if (numberCharactersInPassword > numberCharactersInPasswordInput)
+            {
+                Console.WriteLine("Пароль неверный! Строка слишком короткая.");
+            }
+            else if (numberCharactersInPassword < numberCharactersInPasswordInput)
+            {
+                Console.WriteLine("Пароль неверный! Строка слишком длинная.");
+            }
+            else
+            {
+                Console.WriteLine("Пароль неверный!");
+            }
+        }
+    }
+}
