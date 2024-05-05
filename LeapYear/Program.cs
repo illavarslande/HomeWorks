3ba64950d3d79d598baaecc2bcf867f4cb8597fa
illavarslande <illavarslande@gmail.com>
2024-05-05 17:51:38 +0500

    Задача "Високосный год".

---
+++ LeapYear/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год: ");
+            if (int.TryParse(Console.ReadLine(), out int year))
+            {
+                if (year > 0)
+                {
+                    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                    {
+                        Console.WriteLine("Этот год високосный!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Этот год не високосный!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели отрицательное значение.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели буквы.");
+            }
+        }
+    }
+}
