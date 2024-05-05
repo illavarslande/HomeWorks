8a98d5c808f601db0f1306525bb8c71112834225
illavarslande <illavarslande@gmail.com>
2024-05-06 02:14:02 +0500

    Задача "Площадь треугольника". • Прочитать с консоли координаты трёх точек на плоскости: (x1, y1), (x2, y2), (x3, y3) • Вычислить и вывести на экран площадь данного треугольника • Для вычисления площади можно воспользоваться формулой Герона: • 𝑆∆ = 𝑝(𝑝 − 𝑎)(𝑝 − 𝑏)(𝑝 − 𝑐), где 𝑝 −полупериметр треугольника 𝑝=(𝑎+𝑏+𝑐)/2, 𝑎, 𝑏, 𝑐 −длины сторон треугольника • Проверить на случай, когда эти точки лежат на одной прямой – в этом случае вычислять площадь не нужно, а нужно вывести сообщение об этом

---
+++ AreaOfTriangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AreaOfTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки А (x1, y1): ");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты точки B (x2, y2): ");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты точки C (x3, y3): ");
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+
+            double sideAB = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double sideBC = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2));
+            double sideAC = Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2));
+
+            if ((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) == 0)
+            {
+                Console.WriteLine("Точки лежат на одной прямой.");
+            }
+            else
+            {
+                double semiperimetr = (sideAB + sideBC + sideAC) / 2;
+                double triangleArea = Math.Sqrt(semiperimetr * (semiperimetr - sideAB) * (semiperimetr - sideBC) * (semiperimetr - sideAC));
+                Console.WriteLine($"Площадь треугольника: {triangleArea}");
+            }
+        }
+    }
+}
