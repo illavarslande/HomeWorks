65d4e43d54aad4a5268e5e1cfdb1ce6da23d3c6b
illavarslande <illavarslande@gmail.com>
2024-05-09 02:03:20 +0500

    Задача "Возраст" и "Квадратное уравнение".

---
+++ Age/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свой возраст (от 1 до 112 лет): ");
+            string age = Console.ReadLine();
+
+            if (int.TryParse(age, out int ageInt) && ageInt >= 1 && ageInt <= 112)
+            {
+                char lastDigit = age[age.Length - 1];
+                if (ageInt != 11 && lastDigit == '1' && ageInt != 111)
+                {
+                    Console.WriteLine($"Вам {age} год.");
+                }
+                else if (!(ageInt >= 12 && ageInt <= 14) && ageInt != 112 && (lastDigit >= '2' && lastDigit <= '4'))
+                {
+                    Console.WriteLine($"Вам {age} года.");
+                }
+                else
+                {
+                    Console.WriteLine($"Вам {age} лет.");
+                }
+            }
+            else if (ageInt < 1)
+            {
+                Console.WriteLine("Вы слишком малы.");
+            }
+            else
+            {
+                Console.WriteLine("Вы слишком стары.");
+            }
+        }
+    }
+}
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace QuadraticEquation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первый коэффициент: ");
+            double coefficientFirst = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второй коэффициент: ");
+            double coefficientSecond = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третий коэффициент: ");
+            double coefficientThird = double.Parse(Console.ReadLine());
+
+            const double epsilon = 1e-10;
+
+            if (Math.Abs(coefficientFirst) <= epsilon)
+            {
+                if (Math.Abs(coefficientSecond) <= epsilon)
+                {
+                    if (Math.Abs(coefficientThird) <= epsilon)
+                    {
+                        Console.WriteLine("Уравнение имеет бесконечное количесво корней.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Уравнение не имеет решений.");
+                    }
+
+                }
+                else
+                {
+                    double rootFirst = -coefficientThird / coefficientSecond;
+                    Console.WriteLine($"Это не квадратное уравнение. Но данное уравнение имеет один корень x1 = {rootFirst}");
+                }
+            }
+            else
+            {
+                double discriminant = Math.Pow(coefficientSecond, 2) - 4 * coefficientFirst * coefficientThird;
+
+                if (discriminant > epsilon)
+                {
+                    double rootFirst = (-coefficientSecond + Math.Sqrt(discriminant)) / (2 * coefficientFirst);
+                    double rootSecond = (-coefficientSecond - Math.Sqrt(discriminant)) / (2 * coefficientFirst);
+                    Console.WriteLine($"Уравнение имеет два корня x1 = {rootFirst}, x2 = {rootSecond}");
+                }
+                else if (Math.Abs(discriminant) <= epsilon)
+                {
+                    double rootFirst = -coefficientSecond / (2 * coefficientFirst);
+                    Console.WriteLine($"Уравнение имеет один корень x1 = {rootFirst}");
+                }
+                else
+                {
+                    Console.WriteLine("Корней нет :(");
+                }
+            }
+        }
+    }
+}
