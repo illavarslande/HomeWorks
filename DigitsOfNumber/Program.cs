3e5173397fa9eab1a5fae4cc6cbdf4fa94d81491
illavarslande <illavarslande@gmail.com>
2024-05-16 03:45:40 +0500

    Digits of Number Task

---
+++ DigitsOfNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DigitsOfNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine("Вы ввели не целое число.");
+            }
+            else
+            {
+                int digitsSum = 0;
+                int oddDigitsSum = 0;
+                int maxDigit = 0;
+
+                while (number != 0)
+                {
+                    digitsSum += number % 10;
+
+                    int digit = number % 10;
+                    if (digit % 2 != 0)
+                    {
+                        oddDigitsSum += digit;
+                    }
+
+                    maxDigit = Math.Max(maxDigit, digit);
+
+                    number /= 10;
+                }
+                Console.WriteLine($"Сумма цифр числа равна {digitsSum}");
+                Console.WriteLine($"Сумма нечётных цифр числа равна {oddDigitsSum}");
+                Console.WriteLine($"Максимальная цифра числа {maxDigit}");
+            }
+        }
+    }
+}
