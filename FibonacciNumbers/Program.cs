6127c1887338f6b495aee96c09b6afe94650ff45
illavarslande <illavarslande@gmail.com>
2024-05-16 03:04:20 +0500

    The Fibonacci Numbers Problem

---
+++ FibonacciNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FibonacciNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число n: ");
+
+            double fibonacci_1 = 0;
+            double fibonacci_2 = 1;
+            double numberFibonacci = 0;
+
+            while (true)
+            {
+                if (!double.TryParse(Console.ReadLine(), out double yourNumber))
+                {
+                    Console.WriteLine("Введите корректное значение.");                
+                }
+                else
+                {
+                    for (int i = 2; i <= yourNumber; i++)
+                    {
+                        numberFibonacci = fibonacci_1 + fibonacci_2;
+                        fibonacci_1 = fibonacci_2;
+                        fibonacci_2 = numberFibonacci;
+                    }
+                    Console.WriteLine($"Число Фибоначчи с индексом {yourNumber} - {numberFibonacci}");
+                }
+            }
+        }
+    }
+}
