00cd1c6cd9668a2bbd4945ba639e78251156da1c
illavarslande <illavarslande@gmail.com>
2024-05-18 23:16:33 +0500

    Fix while for FibonacciNumbers

--- FibonacciNumbers/Program.cs
+++ FibonacciNumbers/Program.cs
@@ -6,30 +6,33 @@ namespace FibonacciNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите целое число n: ");
 
+            double number;
             double fibonacci_1 = 0;
             double fibonacci_2 = 1;
             double numberFibonacci = 0;
 
             while (true)
             {
-                if (!double.TryParse(Console.ReadLine(), out double yourNumber))
+                Console.WriteLine("Введите целое число n: ");
+                string inputNumber = Console.ReadLine();
+
+                if (!double.TryParse(inputNumber, out number))
                 {
-                    Console.WriteLine("Введите корректное значение.");                
+                    Console.WriteLine("Введите корректное значение.");
                 }
                 else
                 {
-                    for (int i = 2; i <= yourNumber; i++)
-                    {
-                        numberFibonacci = fibonacci_1 + fibonacci_2;
-                        fibonacci_1 = fibonacci_2;
-                        fibonacci_2 = numberFibonacci;
-                    }
-                    Console.WriteLine($"Число Фибоначчи с индексом {yourNumber} - {numberFibonacci}");
                     break;
                 }
             }
+            for (int i = 2; i <= number; i++)
+            {
+                numberFibonacci = fibonacci_1 + fibonacci_2;
+                fibonacci_1 = fibonacci_2;
+                fibonacci_2 = numberFibonacci;
+            }
+            Console.WriteLine($"Число Фибоначчи с индексом {number} - {numberFibonacci}");
         }
     }
 }
