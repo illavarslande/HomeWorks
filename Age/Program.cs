0ccebbaea99eea6ea6ba71fa3cc490d400dd6b2e
illavarslande <illavarslande@gmail.com>
2024-05-18 23:42:59 +0500

    Checking by last numbers. Problem Age.

--- Age/Program.cs
+++ Age/Program.cs
@@ -11,12 +11,14 @@ namespace Age
 
             if (int.TryParse(age, out int ageInt) && ageInt >= 1 && ageInt <= 112)
             {
-                char lastDigit = age[age.Length - 1];
-                if (ageInt != 11 && lastDigit == '1' && ageInt != 111)
+                int lastDigit = ageInt % 10;
+                int lastTwoDigits = ageInt % 100;
+
+                if (lastTwoDigits != 11 && lastDigit == 1)
                 {
                     Console.WriteLine($"Вам {age} год.");
                 }
-                else if (!(ageInt >= 12 && ageInt <= 14) && ageInt != 112 && (lastDigit >= '2' && lastDigit <= '4'))
+                else if (!(lastTwoDigits >= 12 && lastTwoDigits <= 14) && lastDigit >= 2 && lastDigit <= 4)
                 {
                     Console.WriteLine($"Вам {age} года.");
                 }
---
+++ NextData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NextDate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сегодняшнюю дату: ");
+        }
+    }
+}
