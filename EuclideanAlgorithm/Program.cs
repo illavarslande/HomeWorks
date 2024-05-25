88f478a0a56c2246c5b35808b2ad2571825b2709
illavarslande <illavarslande@gmail.com>
2024-05-25 15:15:04 +0500

    Problem "EuclideanAlgorithm".

---
+++ EuclideanAlgorithm/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EuclideanAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            string numberFirstInput = Console.ReadLine();
+
+            Console.Write("Введите второе число: ");
+            string numberSecondInput = Console.ReadLine();
+
+            if (!int.TryParse(numberFirstInput, out int numberFirst) || !int.TryParse(numberSecondInput, out int numberSecond))
+            {
+                Console.WriteLine("Вы ввели некорректные значения.");
+            }
+            else
+            {
+                int changeNumberFirst = numberFirst;
+                int changeNumberSecond = numberSecond;
+                int nod = 0;
+
+                while (changeNumberFirst % changeNumberSecond != 0)
+                {
+                    int remainder = changeNumberFirst % changeNumberSecond;
+                    changeNumberFirst = numberSecond;
+                    changeNumberSecond = remainder;
+
+                    nod = changeNumberSecond;
+                }
+                Console.WriteLine($"НОД {numberFirst} и {numberSecond} = {nod}");
+            }
+        }
+    }
+}
