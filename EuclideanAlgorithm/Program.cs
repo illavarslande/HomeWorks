109ab8fdbfcc90350d947203f0665c899e27ad02
illavarslande <illavarslande@gmail.com>
2024-05-25 16:04:20 +0500

    Fix variable names. Change while condition. Remove "nod".

--- EuclideanAlgorithm/Program.cs
+++ EuclideanAlgorithm/Program.cs
@@ -7,30 +7,27 @@ namespace EuclideanAlgorithm
         static void Main(string[] args)
         {
             Console.Write("Введите первое число: ");
-            string numberFirstInput = Console.ReadLine();
+            string number1Input = Console.ReadLine();
 
             Console.Write("Введите второе число: ");
-            string numberSecondInput = Console.ReadLine();
+            string number2Input = Console.ReadLine();
 
-            if (!int.TryParse(numberFirstInput, out int numberFirst) || !int.TryParse(numberSecondInput, out int numberSecond))
+            if (!int.TryParse(number1Input, out int number1) || !int.TryParse(number2Input, out int number2))
             {
                 Console.WriteLine("Вы ввели некорректные значения.");
             }
             else
             {
-                int changeNumberFirst = numberFirst;
-                int changeNumberSecond = numberSecond;
-                int nod = 0;
+                int temp1 = number1;
+                int temp2 = number2;
 
-                while (changeNumberFirst % changeNumberSecond != 0)
+                while (temp2 != 0)
                 {
-                    int remainder = changeNumberFirst % changeNumberSecond;
-                    changeNumberFirst = numberSecond;
-                    changeNumberSecond = remainder;
-
-                    nod = changeNumberSecond;
+                    int remainder = temp1 % temp2;
+                    temp1 = temp2;
+                    temp2 = remainder;
                 }
-                Console.WriteLine($"НОД {numberFirst} и {numberSecond} = {nod}");
+                Console.WriteLine($"НОД {number1} и {number2} = {temp1}");
             }
         }
     }
