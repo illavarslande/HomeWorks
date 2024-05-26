72d26063b8ecbb79095bf6e5139b04e7de89a816
illavarslande <illavarslande@gmail.com>
2024-05-26 14:53:18 +0500

    "GuessTheNumber" problem.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = new Random().Next(1, 100);
+            Console.WriteLine(number);
+            int attempts = 1;
+
+            Console.Write("Угадайте число от 1 до 100: ");
+
+            while (true)
+            {
+                string yourNumberInput = Console.ReadLine();
+
+                if (!int.TryParse(yourNumberInput, out int yourNumber) || yourNumber < 1 || yourNumber > 100)
+                {
+                    Console.WriteLine("Вы ввели некорректные значения.");
+                }
+                else
+                {
+                
+                    if (number == yourNumber)
+                    {
+                        Console.WriteLine($"Верно! Количество попыток: {attempts}.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не верно! Попробуйте снова.");
+                        attempts++;
+                    }
+                }
+            }
+        }
+    }
+}
