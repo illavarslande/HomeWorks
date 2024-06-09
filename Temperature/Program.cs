4ec11efdfce9f7c41b6300661823b369f64305a4
illavarslande <illavarslande@gmail.com>
2024-06-09 23:23:11 +0500

    "Temperature" problem.

---
+++ Temperature/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Celsius temperature: ");
+            string temperatureInput = Console.ReadLine();
+
+            bool isInputNumber = int.TryParse(temperatureInput, out int temperature);
+            bool isInputCorrect = temperature >= -273;
+
+            if (!isInputNumber)
+            {
+                Console.WriteLine("Input words, try again");
+                return;
+            }
+            if (!isInputCorrect)
+            {
+                Console.WriteLine("Input correct temperature.");
+                return;
+            }
+
+            Console.WriteLine($"Celsius: {temperature}");
+            Console.WriteLine($"Kelvin: {ToKelvin(temperature)}");
+            Console.WriteLine($"Fahrenheit: {ToFahrenheit(temperature)}");
+        }
+
+        static int ToKelvin(int temperature)
+        {
+            return temperature + 273;
+        }
+
+        static int ToFahrenheit(int temperature)
+        {
+            return temperature + 32;
+        }
+    }
+}
