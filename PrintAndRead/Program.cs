49943e5e03a7949b909047f05425b828b287c69c
illavarslande <illavarslande@gmail.com>
2024-06-09 23:16:17 +0500

    "PrintAndRead" problem.

---
+++ PrintAndRead/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int value1 = PrintAndRead();
+            int value2 = PrintAndRead();
+            int value3 = PrintAndRead();
+
+            Console.WriteLine("Sum: " + (value1 + value2 + value3));
+        }
+
+        static int PrintAndRead()
+        {
+            while (true)
+            {
+                Console.Write("Input value: ");
+                string inputValue = Console.ReadLine();
+                if (!int.TryParse(inputValue, out int value))
+                {
+                    Console.WriteLine("Inpur correct int value.");
+                }
+                else
+                {
+                    return value;
+                    break;
+                }
+            }
+        }
+    }
+}
