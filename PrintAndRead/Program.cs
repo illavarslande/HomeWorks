62a99d466ec2dc45928d75d0a5697f63b9848f91
illavarslande <illavarslande@gmail.com>
2024-07-01 23:49:57 +0500

    Fix PrintAndRead. Input and conversion in PrintAndRead.

--- PrintAndRead/Program.cs
+++ PrintAndRead/Program.cs
@@ -6,19 +6,21 @@ namespace PrintAndRead
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter numbers.");
 
-            int value1 = PrintAndRead();
-            int value2 = PrintAndRead();
-            int value3 = PrintAndRead();
+            int value1 = PrintAndRead("Enter 1 value: ");
+            int value2 = PrintAndRead("Enter 2 value: ");
+            int value3 = PrintAndRead("Enter 3 value: ");
 
             Console.WriteLine("Sum: " + (value1 + value2 + value3));
         }
 
-        static int PrintAndRead()
+        static int PrintAndRead(string enterValues)
         {
             while (true)
             {
-                Console.Write("Input value: ");
+                Console.WriteLine(enterValues);
+
                 string inputValue = Console.ReadLine();
                 if (!int.TryParse(inputValue, out int value))
                 {
@@ -27,7 +29,6 @@ namespace PrintAndRead
                 else
                 {
                     return value;
-                    break;
                 }
             }
         }
