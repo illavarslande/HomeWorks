928ffe8717d4f57046eca414dae4cb218a8a99bb
illavarslande <illavarslande@gmail.com>
2024-07-07 18:50:58 +0500

    Palindrome.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string: ");
+            string text = Console.ReadLine();
+
+            int start = 0;
+            int end = text.Length - 1;
+            while (start < end)
+            {
+                while (start < end && !char.IsLetter(text[start]))
+                {
+                    start++;
+                }
+                while (start < end && !char.IsLetter(text[end]))
+                {
+                    end--;
+                }
+                if (char.ToLower(text[start]) != char.ToLower(text[end]))
+                {
+                    Console.WriteLine("Your text is not palindrome.");
+                    return;
+                }
+                start++;
+                end--;
+            }
+            Console.WriteLine("Yout text is palindrome.");
+        }
+    }
+}
