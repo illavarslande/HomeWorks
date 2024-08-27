79363e37ada3766114eee54c0a79d2b1290b22d6
illavarslande <illavarslande@gmail.com>
2024-08-28 00:13:07 +0500

    Problem MaxSubstring

---
+++ MaxSubstring/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MaxSubstring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string: ");
+            string str = Console.ReadLine();
+            int maxSubstring = 0;
+            int currentSubstring = 1;
+            Console.WriteLine(str.Length);
+
+            for (int i = 0; i <= str.Length - 1; i++)
+            {
+                if ((i + 1 < str.Length) && str[i] == str[i + 1])
+                {
+                    currentSubstring++;
+                }
+                else
+                {
+                    if (currentSubstring > maxSubstring)
+                        maxSubstring = currentSubstring;
+                    currentSubstring = 1;
+                }
+            }
+            Console.WriteLine(maxSubstring);
+        }
+    }
+}
