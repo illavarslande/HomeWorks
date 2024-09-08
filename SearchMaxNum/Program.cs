6b47b72433f69910f13e8899c6dec738e3e12f78
illavarslande <illavarslande@gmail.com>
2024-09-08 22:19:44 +0500

    'SearchMaxNum' problem.

---
+++ SearchMaxNum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SearchMaxNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double[] array = new double[random.Next(10)];
+            double maxNum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(10) + random.NextDouble();
+                Console.Write(array[i]);
+                Console.WriteLine();
+            }
+            maxNum = SearchMaxNum(array, maxNum);
+            Console.WriteLine();
+            Console.WriteLine($"The maximum number of this array is {maxNum}.");
+        }
+
+        static double SearchMaxNum(double[] array, double maxNum)
+        {
+            for (int i = 0;i < array.Length;i++)
+            {
+                if (array[i] > maxNum)
+                {
+                    maxNum = array[i];
+                }
+            }
+            return maxNum;
+        }
+    }
+}
+
