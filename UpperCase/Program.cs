16a7f329a08483cd5d51c37c44e4534494da79dd
illavarslande <illavarslande@gmail.com>
2024-09-15 01:51:58 +0500

    UpperCase problem

---
+++ UpperCase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UpperCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Eren", "Armin", "Mikasa" };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+            Console.WriteLine();
+
+            array = ToUpperCase(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+
+        static string[] ToUpperCase (string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i].ToUpper();
+            }
+            return array;
+        }
+    }
+}
