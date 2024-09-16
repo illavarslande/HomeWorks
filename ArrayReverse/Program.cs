6661bea5eabfa0d06f911a4b67329d8f1071e360
illavarslande <illavarslande@gmail.com>
2024-09-16 22:54:29 +0500

    ArrayReverse

---
+++ ArrayReverse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArrayReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+    }
+}
