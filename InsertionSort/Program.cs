baf740f82f67f5da6538c5223edefa6219639699
illavarslande <illavarslande@gmail.com>
2024-10-28 01:43:28 +0500

    Problem 'InsertionSort'

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 4, 6, 5, 9, 11, 2, 0 };
+
+            Console.Write("Array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}  ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int temp = array[i];
+                int j = i - 1;
+                
+                while (j >= 0 && temp < array[j])
+                {
+                    array[j + 1]= array[j];
+                    j--;
+                }
+
+                array[j + 1] = temp;
+            }
+
+            Console.Write("Array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}  ");
+            }
+        }
+    }
+}
