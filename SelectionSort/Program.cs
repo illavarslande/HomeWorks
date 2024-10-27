f9d1a4e3229ce3958a877567b95b188e4a15f596
illavarslande <illavarslande@gmail.com>
2024-10-27 23:27:04 +0500

    problem 'SelectionSort'

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 43, 5, 0, 13, 22, 14, 45 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                if (min != i)
+                {
+                    int temp = array[i];
+                    array[i] = array[min];
+                    array[min] = temp;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}  ");
+            }
+        }
+    }
+}
