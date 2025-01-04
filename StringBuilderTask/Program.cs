e9cc27b54b6be63511c294ef8e692f7c5b929f14
illavarslande <illavarslande@gmail.com>
2025-01-05 00:29:00 +0500

    StringBuilderTask

---
+++ StringBuilderTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 101; i++)
+            {
+                sb = sb.Append(i).Append(",");
+            }
+            int endIndex = sb.Length - 1;
+            sb = sb.Remove(endIndex, 1);
+            Console.WriteLine(sb);
+        }
+    }
+}
