393c25373bbd3fe67449a65decfb969b466a1f12
illavarslande <illavarslande@gmail.com>
2025-01-06 13:42:27 +0500

    StringSplitting task

---
+++ StringSplitting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringSplitting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = "1, 2, 3, 4, 5";
+            string[] subs = numbers.Split(',');
+            int[] result = new int[subs.Length];
+            int sum = 0;
+
+            for (int i = 0; i < subs.Length; i++)
+            {
+                if (int.TryParse(subs[i], out int number))
+                {
+                    result[i] = number;
+                    sum += number;
+                    Console.Write(result[i]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
