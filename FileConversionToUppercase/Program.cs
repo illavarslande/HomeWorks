e26b25b226d39a77ee58efa3705bb72e01051af3
illavarslande <illavarslande@gmail.com>
2025-01-06 22:17:44 +0500

    FileConversationToUppercase ))))))))

---
+++ FileConversionToUppercase/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace FileConversationToUppercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathForReader = "C:\\Users\\illav\\source\\repos\\HomeWorks\\FileConversionToUppercase\\note1.txt";
+            string pathForWriter = "C:\\Users\\illav\\source\\repos\\HomeWorks\\FileConversionToUppercase\\note2.txt";
+            string text;
+            using (StreamReader reader = new StreamReader(pathForReader))
+            {
+                text = reader.ReadToEnd().ToUpper();
+                Console.WriteLine(text);
+            }
+            using (StreamWriter writer = new StreamWriter(pathForWriter, false))
+            {
+                writer.WriteLine(text);
+            }
+        }
+    }
+}
