e40fc540fcbf8f5d056cc372248e1bb780229685
illavarslande <illavarslande@gmail.com>
2025-03-09 21:16:39 +0500

    'Range'

--- Range/Program.cs
+++ Range/Program.cs
@@ -3,52 +3,18 @@ using System.Runtime.InteropServices;
 
 namespace Range
 {
-    class Range
-    {
-        public int from;
-        public int to;
-
-        public int LengthRange()
-        {
-            return to - from;
-        }
-
-        public bool isInside(int num)
-        {
-            bool result = num < to && num > from;
-            if (result)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Range range = new Range();
+            Range range = new Range(-5, 10);
+            Console.WriteLine($"Range1: {range.from}, {range.to}");
 
-            string fromInput = Console.ReadLine();
-            if (!int.TryParse(fromInput, out range.from))
-            {
-                Console.WriteLine("Invalid from number.");
-                return;
-            }
+            Range range1 = new Range(-6, 10);
+            Console.WriteLine($"Range2: {range1.from}, {range1.to}");
 
-            string toInput = Console.ReadLine();
-            if (!int.TryParse(toInput, out range.to))
-            {
-                Console.WriteLine("Invalid to number.");
-                return;
-            }
-
-            int lengthRange = range.LengthRange();
-            Console.WriteLine($"LengthRange: {lengthRange}");
+            int length = range.Length();
+            Console.WriteLine($"Length Range1: {length}");
 
             Console.WriteLine("Enter a number: ");
             string numInput = Console.ReadLine();
@@ -58,8 +24,30 @@ namespace Range
             }
             else
             {
-                bool isInside = range.isInside(num);
+                bool isInside = range.IsInside(num);
                 Console.WriteLine(isInside);
+            }
+
+            Range intersection = range.GetIntersection(range1);
+            if (intersection == null)
+            {
+                Console.WriteLine($"Intersection: {null}");
+            }
+            else
+            {
+                Console.WriteLine($"Intersection: {intersection.from}, {intersection.to}");
+            }
+
+            Range[] getSum = range.GetUnion(range1);
+            foreach (var sumRange in getSum)
+            {
+                Console.WriteLine($"Sum: [{sumRange.from}, {sumRange.to}]");
+            }
+
+            Range[] getDifference = range.GetDifference(range1);
+            foreach (var differenceRange in getDifference)
+            {
+                Console.WriteLine($"Difference: [{differenceRange.from}, {differenceRange.to}]");
             }
         }
     }
---
+++ Range/Range.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using System.Net.Http.Headers;
+
+namespace Range
+{
+    class Range
+    {
+        public int from { get; private set; }
+        public int to { get; private set; }
+        public Range(int from, int to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public int Length()
+        {
+            return to - from;
+        }
+
+        public bool IsInside(int num)
+        {
+            bool result = num <= to && num >= from;
+            return result;
+        }
+
+        public Range? GetIntersection(Range range)
+        {
+            int newFrom = Math.Max(from, range.from);
+            int newTo = Math.Min(to, range.to);
+            if (newFrom <= newTo)
+            {
+                return new Range(newFrom, newTo);
+            }
+            return null;
+        }
+
+/*        public Range[] GetUnion(Range range)
+        {
+            if (IsInside(range.from) || IsInside(range.to))
+            {
+                int newFrom = from + range.from;
+                int newTo = to + range.to;
+                return new Range[] { new Range(newFrom, newTo) };
+            }
+            return new Range[] { new Range(from, to), new Range(range.from, range.to) };
+        }
+
+            public Range[] GetDifference(Range range)
+        {
+            if (IsInside(range.from) || IsInside(range.to))
+            {
+                int newFrom = from - range.from;
+                int newTo = to - range.to;
+                return new Range[] { new Range(newFrom, newTo) };
+            }
+            return new Range[] { new Range(from, to), new Range(range.from, range.to) };
+        }
+*/
+        public Range[] GetUnion(Range range)
+        {
+            if (IsInside(range.from) || IsInside(range.to))
+            {
+                int newFrom = Math.Min(from, range.from);
+                int newTo = Math.Max(to, range.to);
+                return new Range[] { new Range(newFrom, newTo) };
+            }
+            return new Range[] { new Range(from, to), new Range(range.from, range.to) };
+        }
+
+        public Range[] GetDifference(Range range)
+        {
+            if (to <= range.from || from >= range.to)
+            {
+                return new Range[] { new Range(from, to) };
+            }
+            if (range.from < from && range.to >= to)
+            {
+                return new Range[0];
+            }
+
+            List<Range> result = new List<Range>();
+
+            if (from < range.from)
+            {
+                result.Add(new Range(from, range.from));
+            }
+            if (to > range.to)
+            {
+                result.Add(new Range(range.to, to));
+            }
+
+            return result.ToArray();
+        }
+
+    }
+
+}
