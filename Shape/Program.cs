d219a64b18a8a22e3db758b091c8c626ef104ea5
illavarslande <illavarslande@gmail.com>
2025-03-16 12:19:54 +0500

    Shape: Circle, Rectungle

---
+++ Shape/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Shape
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            circle.PrintName();
+            Console.WriteLine($"Area: {circle.GetArea()}");
+            Console.WriteLine($"Perimeter: {circle.GetPerimeter()}");
+
+            Console.WriteLine();
+
+           Rectangle rectangle = new Rectangle(3, 6);
+            rectangle.PrintName();
+            Console.WriteLine($"Area: {rectangle.GetArea()}");
+            Console.WriteLine($"Perimeter: {rectangle.GetPerimeter()}");
+        }
+    }
+}
---
+++ Shape/Shape.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Shape
+{
+    interface IShape
+    {
+        void PrintName();
+        abstract double GetArea();
+        abstract double GetPerimeter();
+    }
+
+    abstract class Shape : IShape
+    {
+        protected string _name { get; }
+
+        public Shape(string name) 
+        {
+            _name = name;
+        }
+        public void PrintName() 
+        {
+            Console.WriteLine($"Shape is {_name}"); 
+        }
+        public abstract double GetArea();
+
+        public abstract double GetPerimeter();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; }
+        public Circle(double radius) : base("Circle")
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea() => Math.PI * Radius * Radius;
+        public override double GetPerimeter() => Math.PI * Radius * 2;
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Height { get; }
+        public Rectangle(double width, double heigth) : base("Rectangle")
+        {
+            Width = width;
+            Height = heigth;
+        }
+        public override double GetArea() => Width * Height;
+        public override double GetPerimeter() => (Height + Width) * 2;
+    }
+}
