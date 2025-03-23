ea380daa96841dd084b9fe434f295dcd7f6b32f8
illavarslande <illavarslande@gmail.com>
2025-03-23 20:08:02 +0500

    Vector

---
+++ Vector/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new Vector(new float[] { 2, 5, 8 });
+            Vector vector2 = new Vector(new float[] { -2, 0, 7 });
+
+            Vector sum = vector1.Add(vector2);
+            sum.Print();
+            Console.WriteLine();
+
+            Vector substract = vector1.Subsrtact(vector2);
+            substract.Print();
+            Console.WriteLine();
+
+            Vector multiplyByScalar = vector1.MultiplyByScalar(2);
+            multiplyByScalar.Print();
+            Console.WriteLine();
+
+            float dotProduct = vector1.DotProduct(vector2);
+            Console.WriteLine(dotProduct);
+        }
+    }
+}
---
+++ Vector/Vector.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Vector
+{
+    internal class Vector
+    {
+        float[] data;
+        int Size { get; }
+
+        public Vector(int size)
+        {
+            Size = size;
+            data = new float[size];
+        }
+
+        public Vector(float[] values)
+        {
+            Size = values.Length;
+            data = new float[Size];
+            Array.Copy(values, data, Size);
+        }
+
+        public float Get(int index)
+        {
+            return data[index];
+        }
+
+        public void Set(int index, float value) 
+        { 
+            data[index] = value; 
+        }
+
+        public Vector Add(Vector other)
+        {
+            if (Size != other.Size)
+            {
+                throw new ArgumentException("The size of the arrays is not the same");
+            }
+
+            float[] resultVector = new float[Size];
+            for (int i = 0; i < Size; i++)
+            {
+                resultVector[i] = data[i] + other.data[i];
+            }
+            return new Vector(resultVector);
+
+        }
+
+        public Vector Subsrtact(Vector other) 
+        {
+            if (Size != other.Size)
+            {
+                throw new ArgumentException("The size of the arrays is not the same");
+            }
+
+            float[] resultVector = new float[Size];
+            for (int i = 0; i < Size; i++)
+            {
+                resultVector[i] = data[i] - other.data[i];
+            }
+            return new Vector(resultVector);
+        }
+
+        public Vector MultiplyByScalar(float scalar)
+        {
+
+            float[] resultVector = new float[Size];
+            for (int i = 0; i < Size; i++)
+            {
+                resultVector[i] = data[i] * scalar;
+            }
+            return new Vector(resultVector);
+        }
+
+        public float DotProduct(Vector other)
+        {
+            float result = 0;
+            for (int i = 0; i < Size; i++)
+            {
+                result += data[i] * other.data[i];
+            }
+            return result;
+        }
+
+        public void Print()
+        {
+            Console.Write("Vector: ");
+            for (int i = 0; i < Size; i++)
+            {
+                Console.Write($"{data[i]} ");
+            }
+        }
+    }
+}
